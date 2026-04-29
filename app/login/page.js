c9eccebd57f3857211d25958a5d2e8c8cc98bcef
Dare187 <oamenisaac6@gmail.com>
2026-04-29 16:06:14 +0100

    first commit

---
+++ app/login/page.js
@@ -0,0 +1,89 @@
+'use client'
+import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import { ToastContainer, toast } from 'react-toastify';
+
+const Login = () => {
+    const router = useRouter();
+    const [data, setData] = useState({
+            email: '',
+            password: ''
+        });
+
+            const handleSubmit = async (e) =>{
+        e.preventDefault();
+        console.log('form submitted'); // add this
+        console.log(data);             // add this
+
+        const { email, password } = data;
+
+        if (password === '' || email ==='') {
+            toast.error('fill all fields!');
+            return;
+        }
+
+        if (password.length < 8) {
+            toast.error('password must be at least 8 characters!');
+            return;
+        }
+        try {
+            const res = await signIn('credentials', {email, password, redirect: false});
+            console.log(res); // add this
+
+            if (res && !res.error) {
+                router.replace('/');
+            } else {
+                toast.error('error logging in!');
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
+
+    return (
+        <section className='flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10 mx-auto mt-24 bg-gray-900'>
+        <h2 className='self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl'>Login</h2>
+        <form onSubmit={handleSubmit}>
+            <div className='flex flex-col mb-2'>
+            <div className='flex relative'>
+            <input
+            className='rounded-md inline-flex items-center px-3 border-t bg-white border border-b border-gray-300 text-gray-500 shadow-sm text-sm w-full p-4'
+                placeholder='Email'
+                name='email'
+                id='email'
+                type='email'
+                value={data.email}
+                onChange={(e) => setData({ ...data, email: e.target.value })}
+            />
+            </div>
+            </div>
+            <div className='flex flex-col mb-2'>
+            <div className='flex relative'>
+            <input
+              className='rounded-md inline-flex items-center px-3 border-t bg-white border border-b border-gray-300 text-gray-500 shadow-sm text-sm w-full p-4'
+               placeholder='password'
+                name='password'
+                id='password'
+                type='password'
+                value={data.password} 
+                onChange={(e) => {setData({ ...data, password: e.target.value });
+              }}
+            />
+                </div>
+                </div>
+            <button
+    type="button"
+    onClick={handleSubmit}
+    className='py-2 px-4 bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-sm'
+>
+    Login
+</button>
+        </form>     
+       </section>
+    );
+}
+
+export default Login
